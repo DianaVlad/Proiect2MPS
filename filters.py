2a743ccfc8f5ce7b9c580a58aa8c105bc524ca56
Sebastian-Andrei MATEI?A <sebastian.mateita@cs.pub.ro>
2020-01-05 15:20:34 +0200

    Finished work on image filtering

--- filters.py
+++ filters.py
@@ -1,61 +1,84 @@
 import cv2 as cv
-from PIL import Image, ImageFilter
 import numpy as np
-import matplotlib.pyplot as pltplt
+import copy
 
-img = cv.imread('christinaaguilera.jpg') # Importing Sample Test Image
+def applyFilters(image, filterList):
+	grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
+	while len(filterList) > 0:
+		newFilter = filterList.pop(0)
+		
+		if newFilter == 'edgepreserve':
+			image = cv.edgePreservingFilter(image, flags=1, sigma_s=60, sigma_r=0.4)
+		
+		if newFilter == 'pencilsketch':
+			dst_gray, image = cv.pencilSketch(image, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
+			
+		if newFilter == 'water':
+			image = cv.stylization(image, sigma_s=60, sigma_r=0.07)
+			
+		if newFilter == 'grayscale':
+			image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+			
+		if newFilter == 'blackwhite':
+			(thresh, image) = cv.threshold(grayImage, 127, 255, cv.THRESH_BINARY)
+			
+		if newFilter == 'jet':
+			image = cv.applyColorMap(grayImage, cv.COLORMAP_JET)
+		
+		if newFilter == 'hot':
+			image = cv.applyColorMap(grayImage, cv.COLORMAP_HOT)
+		
+		if newFilter == 'sepia':
+			i, j, k = image.shape
+			originalImage = copy.deepcopy(image)
+			
+			for x in range(i):
+				for y in range(j):
+					R = originalImage[x,y,2] * 0.393 + originalImage[x,y,1] * 0.769 + originalImage[x,y,0] * 0.189
+					G = originalImage[x,y,2] * 0.349 + originalImage[x,y,1] * 0.686 + originalImage[x,y,0] * 0.168
+					B = originalImage[x,y,2] * 0.272 + originalImage[x,y,1] * 0.534 + originalImage[x,y,0] * 0.131
+					
+					if R > 255:
+						image[x,y,2] = 255
+					else:
+						image[x,y,2] = R
+         
+					if G > 255:
+						image[x,y,1] = 255
+					else:
+						image[x,y,1] = G
+         
+					if B > 255:
+						image[x,y,0] = 255
+					else:
+						image[x,y,0] = B
+	
+		if newFilter == 'enhance':
+			image = cv.detailEnhance(image)
+			
+		if newFilter == 'blur':
+			image = cv.blur(image, (10, 10))
+			
+		if newFilter == 'blue':
+			blueImage = np.full(image.shape, (255,0,0), np.uint8)
+			image = cv.addWeighted(image, 0.75, blueImage, 0.25, 0)
+		
+		if newFilter == 'green':
+			greenImage = np.full(image.shape, (0,255,0), np.uint8)
+			image = cv.addWeighted(image, 0.75, greenImage, 0.25, 0)
+		
+		if newFilter == 'red':
+			redImage = np.full(image.shape, (0,0,255), np.uint8)
+			image = cv.addWeighted(image, 0.75, redImage, 0.25, 0)
+		
+	return image
+	
+#image = cv.imread('christinaaguilera.jpg') # Importing Sample Test Image
+	
+#image = applyFilters(image, ['red', 'blur'])
 
-### Edge Preserving Filter #####
-# edge = cv.edgePreservingFilter(img, flags=1, sigma_s=60, sigma_r=0.4)
-#
-# ### Pencil Sketch ########
-# dst_gray, dst_color = cv.pencilSketch(img, sigma_s=60, sigma_r=0.07, shade_factor=0.05)
-#
-# ### Stilizare Water Color ######
-# water = cv.stylization(img, sigma_s=60, sigma_r=0.07)
-
-### Black and White ####
-# im_gray = cv.imread('christinaaguilera.jpg', cv.IMREAD_GRAYSCALE)
-# (thresh, im_bw) = cv.threshold(im_gray, 128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
-# thresh = 127
-#im_bw = cv.threshold(im_gray, thresh, 255, cv.THRESH_BINARY)[1]
-
-### Just grayscale ###
-#im_gray = cv.imread('christinaaguilera.jpg', cv.IMREAD_GRAYSCALE)
-
-### Extra Saturated Colors
-#jet_color = cv.applyColorMap(im_gray, cv.COLORMAP_JET)
-
-### Culori Calde ###
-#hot_color = cv.applyColorMap(im_gray, cv.COLORMAP_HOT)
-
-### Sepia #####
-# i, j, k = img.shape
-# newImage = cv.imread('christinaaguilera.jpg')
-# for x in range(i):
-#     for y in range(j):
-#         R = img[x,y,2] * 0.393 + img[x,y,1] * 0.769 + img[x,y,0] * 0.189
-#         G = img[x,y,2] * 0.349 + img[x,y,1] * 0.686 + img[x,y,0] * 0.168
-#         B = img[x,y,2] * 0.272 + img[x,y,1] * 0.534 + img[x,y,0] * 0.131
-#         if R > 255:
-#             newImage[x,y,2] = 255
-#         else:
-#             newImage[x,y,2] = R
-#         if G > 255:
-#             newImage[x,y,1] = 255
-#         else:
-#             newImage[x,y,1] = G
-#         if B > 255:
-#             newImage[x,y,0] = 255
-#         else:
-#             newImage[x,y,0] = B
-
-### Enhance #####
-enhance = cv.detailEnhance(img)
-
-
-cv.imwrite('./final.jpg', enhance)
+#cv.imwrite('./final.jpg', image)
 
 
 
